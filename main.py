f83d75d7249e15a0785b5e67ea78ad581eb0d6ce
Antonio <yoyominoo7@gmail.com>
2025-11-10 22:35:14 +0100

    Update 1.0.0

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-
-if not TOKEN:
-    raise ValueError("Devi impostare la variabile d'ambiente TELEGRAM_TOKEN!")
-
-# Funzioni dei comandi
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ciao! Sono vivo su Render 🚀")
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("/start - avvia il bot\n/help - mostra comandi disponibili")
+from app import build_application
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-
-    # Aggiungi i comandi
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
-
-    # Avvio in polling
+    app = build_application()
     app.run_polling()
 
 if __name__ == "__main__":
